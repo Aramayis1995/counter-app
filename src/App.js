52a2de66801d31b735afd2dc9dcc292719a60ba1
Aramayis <mayissargsyan1995@gmail.com>
2021-07-18 20:52:31 +0400

    feat:App component added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import "./App.css";
+import "./components/IncreaseButton/IncreaseButton";
+import Button from "./components/IncreaseButton/IncreaseButton";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      maxCount: 10,
+      minCount: 2,
+      count: 0,
+    };
+  }
+
+  getCount = (num) => {
+    if (num === 0) {
+      this.setState({
+        count: 0,
+      });
+    } else if (!(this.state.count === 0 && num === -1)) {
+      this.setState({
+        count: this.state.count + num,
+      });
+    }
+  };
+  changeMaxCount = (event) => {
+    console.log(event);
+
+    this.setState({
+      maxCount: event.target.value,
+    });
+  };
+  render() {
+    let count = this.state.count;
+    return (
+      <div>
+        <h1>Count:{count}</h1>
+        <input value={this.state.minCount}></input>
+        <input
+          value={this.state.maxCount}
+          onChange={this.changeMaxCount}
+        ></input>
+        <Button sign="+" operation={() => this.getCount(+1)}></Button>
+        <Button sign="-" operation={() => this.getCount(-1)}></Button>
+        <Button sign="Reset" operation={() => this.getCount(0)}></Button>
+      </div>
+    );
+  }
 }
-
 export default App;
